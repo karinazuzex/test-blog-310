71966ffc50c2b7a25bb0dc8bd94873cb819a1cae
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-04 00:28:26 +0200

    home-promo: add basic elements

--- pages/index/sections/Promo/index.js
+++ pages/index/sections/Promo/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 
+import { Container, Row } from "components/grid";
+import Button from "components/ui/Button";
+
 const PromoSection = () => (
-    <section>
-        some
+    <section className="section section--home-promo bg-black">
+        <Container>
+            <div className="block text-center text-white">
+                <h1 className="block__title block__elem--lg">
+                    Introducing Memurai<br />
+                    Redis-compatible, enterprise-grade  in-memory database for Windows
+                </h1>
+                <p className="block_description block__elem">
+                    Technical preview now available for download.
+                </p>
+                <Row theme="no-col" className="justify-center-xs">
+                    <Button type="solid" theme="red-white">
+                        Download for free
+                    </Button>
+                    <Button type="hollow" theme="red-white">
+                        Learn more
+                    </Button>
+                </Row>
+                <p className="block__postfix">
+                    Current version: 0.1.2-beta. Compatible with Redis 4.2
+                </p>
+            </div>
+        </Container>
     </section>
 );
 
