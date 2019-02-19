9b784321bb8b24a761652c8c94c3a260fd01a9c6
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-19 20:08:03 +0200

    about us: add page

---
+++ pages/about/index.js
@@ -0,0 +1,13 @@
+import Layout from "components/Layout"
+
+import PromoSection from "sections/Promo/About";
+import SubscribeSection from "sections/Subscribe";
+
+const ContactPage = () => (
+    <Layout theme="white">
+        <PromoSection />
+        <SubscribeSection />
+    </Layout>
+);
+
+export default ContactPage;
---
+++ sections/Promo/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Container } from "components/grid/index";
+
+const PromoSection = () => (
+    <section className="section section__promo section__promo--about">
+        <Container>
+            <div className="block text-center">
+                <h3 className="block__title block__elem text-bold">
+                    About Us
+                </h3>
+                <p className="block_description">
+                    The Memurai team is comprised of individuals who are passionate about delivering cutting edge
+                    solutions that solve customer problems. Our journey with Memurai can be traced back to 2013, when a
+                    few of our team members started working on the Microsoft Open Technologies port of Redis to Windows.
+                    A few years later, after that project had ended, it became clear that there were many companies with
+                    large investments in Windows infrastructure and know-how who needed a high-performance data store
+                    solution compatible with Redis and its ecosystem. Memurai for Windows was born.
+                </p>
+            </div>
+        </Container>
+    </section>
+);
+
+export default PromoSection;
