2350d25d8d8c412aed8d49db4cd5e4cc45056b2e
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-19 21:32:04 +0200

    privacy: add dummy page

---
+++ pages/privacy/index.js
@@ -0,0 +1,11 @@
+import Layout from "components/Layout"
+
+import LegalSection from "sections/Legal/Privacy";
+
+const TermsPage = () => (
+    <Layout theme="white">
+        <LegalSection />
+    </Layout>
+);
+
+export default TermsPage;
