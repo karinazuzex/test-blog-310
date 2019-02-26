35b7fd5b325aa5d70f1c200cfa741ef8e00ad8e1
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-26 19:25:55 +0200

    social: add external links

--- components/Social/index.js
+++ components/Social/index.js
@@ -1,22 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { TWITTER_LINK, FACEBOOK_LINK, GITHUB_LINK, LINKEDIN_LINK } from "config/consts";
+
 import { Row } from "components/grid";
 import Link from "components/ui/Link";
 import { FacebookLogo, LinkedInLogo, GithubLogo, TwitterLogo } from "svg";
 
 const Social = ({ theme = "" }) => (
     <Row className={`justify-center-xs text-center social ${theme ? `social--${theme}` : ""}`}>
-        <Link href="" className="social__item social__item--twitter">
+        <Link
+            href={TWITTER_LINK}
+            rel="noreferrer noopener"
+            target="_blank"
+            className="social__item social__item--twitter"
+        >
             <TwitterLogo />
         </Link>
-        <Link href="" className="social__item social__item--linkedin">
+        <Link
+            href={LINKEDIN_LINK}
+            rel="noreferrer noopener"
+            target="_blank"
+            className="social__item social__item--linkedin"
+        >
             <LinkedInLogo />
         </Link>
-        <Link href="" className="social__item social__item--github">
+        <Link
+            href={GITHUB_LINK}
+            rel="noreferrer noopener"
+            target="_blank"
+            className="social__item social__item--github"
+        >
             <GithubLogo/>
         </Link>
-        <Link href="" className="social__item social__item--facebook">
+        <Link
+            href={FACEBOOK_LINK}
+            rel="noreferrer noopener"
+            target="_blank"
+            className="social__item social__item--facebook"
+        >
             <FacebookLogo />
         </Link>
     </Row>
--- config/consts.js
+++ config/consts.js
@@ -1,5 +1,10 @@
 export const MAILCHIMP_ACTION_URL = "";
 export const MAILCHIMP_HIDDEN_INPUT_NAME = "";
 
+export const TWITTER_LINK = "https://twitter.com/";
+export const LINKEDIN_LINK = "https://www.linkedin.com/";
+export const GITHUB_LINK = "https://github.com/";
+export const FACEBOOK_LINK = "https://www.facebook.com/";
+
 export const EMAIL_PATTERN =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
