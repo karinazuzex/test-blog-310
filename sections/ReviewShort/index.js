0d9ac64e87cef199ff96022fcebdeeac4c2835f5
Serhii Slobodian <beinc.dn@gmail.com>
2019-02-26 22:05:28 +0200

    review: format quote #15

--- sections/ReviewShort/index.js
+++ sections/ReviewShort/index.js
@@ -7,8 +7,9 @@ const ReviewsSection = () => (
     <section className="section section__review-short">
         <Container>
             <div className="block text-center">
-                <h4 className="block__title block__elem">
-                    "With Memurai, we just love how it integrates with Windows which gives us a competitive advantage.”
+                <h4 className="block__title block__elem text-light">
+                    &quot;With Memurai, we just love how it integrates with Windows which gives us a competitive
+                    advantage.&quot;
                 </h4>
             </div>
             <UserPromo />
