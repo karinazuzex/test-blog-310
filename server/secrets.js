906d643a0ed162eba9d5a2aa52f4ea68edd83bde
Alexis Campailla <alexis@janeasystems.com>
2019-02-27 16:11:02 +0100

    Simple secrets management

---
+++ server/secrets.js
@@ -0,0 +1,13 @@
+
+function getVar(key, defaultValue) {
+  if(key in process.env)
+    return process.env[key];
+  else
+    return defaultValue;
+}
+
+/* Provide  default values here that are safe to share on github, for dev/testing */
+/* For production, or otherwise security-sensitive values, use environment variables */
+
+module.exports.sample = getVar('sample', 'myvalue');
+
