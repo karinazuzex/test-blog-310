62afc0d5545d8f5c57bc8f952f69757f1958d8f2
Alexis Campailla <alexis@janeasystems.com>
2019-03-12 18:20:58 +0100

    Add terms of service

--- sections/Legal/Terms/index.js
+++ sections/Legal/Terms/index.js
@@ -13,47 +13,42 @@ const LegalSection = () => (
                     </h3>
                 </div>
                 <div className="legal-block">
-                    <p className="legal-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur dolorem
-                        excepturi, exercitationem illum odio officia rerum similique totam? Assumenda blanditiis debitis
-                        eveniet hic impedit itaque minima perspiciatis quidem. Commodi deserunt doloribus eos nobis,
-                        repellendus reprehenderit. Cupiditate earum eveniet quidem velit voluptatem. Libero minima nostrum
-                        ratione rerum sed, ut.
-                    </p>
-                    <p className="legal-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consectetur dolorem
-                        excepturi, exercitationem illum odio officia rerum similique totam? Assumenda blanditiis debitis
-                        eveniet hic impedit itaque minima perspiciatis quidem. Commodi deserunt doloribus eos nobis,
-                        repellendus reprehenderit. Cupiditate earum eveniet quidem velit voluptatem. Libero minima nostrum
-                        ratione rerum sed, ut.
-                    </p>
-                </div>
-                <div className="legal-block">
-                    <h5 className="legal-subheader">
-                        Lorem ipsum dolor sit.
-                    </h5>
-                    <div className="legal-block">
-                        <ul className="list list--lines">
-                            <li className="list__item">Lorem ipsum dolor sit.</li>
-                            <li className="list__item">Lorem ipsum dolor sit.</li>
-                            <li className="list__item">Lorem ipsum dolor sit.</li>
-                            <li className="list__item">Lorem ipsum dolor sit.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="legal-block">
-                    <Link theme="red">
-                        Some link
-                    </Link>
-                </div>
-                <div className="legal-block">
-                    <ol className="list list--ordered">
-                        <li className="list__item">Lorem ipsum dolor sit.</li>
-                        <li className="list__item">Lorem ipsum dolor sit.</li>
-                        <li className="list__item">Lorem ipsum dolor sit.</li>
-                        <li className="list__item">Lorem ipsum dolor sit.</li>
-                    </ol>
-                </div>
+<p className="legal-paragraph">Last updated: March 12, 2019</p>
+
+
+<p className="legal-paragraph">Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the https://www.memurai.com website (the "Service") operated by Janea Systems, Inc. d/b/a Memurai ("us", "we", or "our").</p>
+
+<p className="legal-paragraph">Your access to and use of the Service is conditioned upon your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who wish to access or use the Service.</p>
+
+<p className="legal-paragraph">By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you do not have permission to access the Service.</p>
+
+</div>
+
+<div className="legal-block">
+
+<h5 className="legal-subheader">Purchases</h5>
+
+<p className="legal-paragraph">If you wish to purchase any product or service made available through the Service ("Purchase"), you may be asked to supply certain information relevant to your Purchase including, without limitation, your credit card number, the expiration date of your credit card, your billing address, and your shipping information.</p>
+
+<p className="legal-paragraph">You represent and warrant that: (i) you have the legal right to use any credit card(s) or other payment method(s) in connection with any Purchase; and that (ii) the information you supply to us is true, correct and complete.</p>
+
+<p className="legal-paragraph">The service may employ the use of third party services for the purpose of facilitating payment and the completion of Purchases. By submitting your information, you grant us the right to provide the information to these third parties subject to our Privacy Policy.</p>
+
+<p className="legal-paragraph">We reserve the right to refuse or cancel your order at any time for reasons including but not limited to: product or service availability, errors in the description or price of the product or service, error in your order or other reasons.</p>
+
+<p className="legal-paragraph">We reserve the right to refuse or cancel your order if fraud or an unauthorized or illegal transaction is suspected.</p>
+
+</div>
+
+<div className="legal-block">
+
+<h5 className="legal-subheader">Availability, Errors and Inaccuracies</h5>
+
+<p className="legal-paragraph">We are constantly updating product and service offerings on the Service. We may experience delays in updating information on the Service and in our advertising on other web sites. The information found on the Service may contain errors or inaccuracies and may not be complete or current. Products or services may be mispriced, described inaccurately, or unavailable on the Service and we cannot guarantee the accuracy or completeness of any information found on the Service.</p>
+
+<p className="legal-paragraph">We therefore reserve the right to change or update information and to correct errors, inaccuracies, or omissions at any time without prior notice.</p>
+
+            </div>
             </div>
         </Container>
     </section>
