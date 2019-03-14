3f3f172d8cda6c57b63da35ad0be3317bf7c3bb0
Serhii Slobodian <beinc.dn@gmail.com>
2019-03-14 21:50:02 +0200

    feature: update next export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
 
 module.exports = withCSS(withSass({
     webpack (config, options) {
         config.module.rules.push({
             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
             use: {
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
                     limit: 100000
                 }
@@ -18,13 +18,14 @@ module.exports = withCSS(withSass({
 
     exportPathMap: function () {
       return {
-        '/': { page: '/' },
-        '/about': { page: '/about' },
-        '/why-memurai': { page: '/why-memurai' },
-        '/get-memurai': { page: '/get-memurai' },
-        '/contact': { page: '/contact' },
-        '/terms': { page: '/terms' },
-        '/privacy': { page: '/privacy' }
+        "/": { page: "/" },
+        "/about": { page: "/about" },
+        "/why-memurai": { page: "/why-memurai" },
+        "/get-memurai": { page: "/get-memurai" },
+        "/contact": { page: "/contact" },
+        "/terms": { page: "/terms" },
+        "/privacy": { page: "/privacy" },
+        "/cookie": { page: "/cookie" }
       }
     }
 }));
