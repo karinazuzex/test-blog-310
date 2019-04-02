f2849512acfe59cf41cae930d7addc0d679f4f54
Serhii Slobodian <beinc.dn@gmail.com>
2019-04-02 23:00:03 +0300

    feature: update reviews section display/hide options #35

--- config/index.js
+++ config/index.js
@@ -2,3 +2,4 @@ export * as routes from "./routes";
 export * as exceptions from "./exceptions";
 export * as consts from "./consts";
 export * as messages from "./messages";
+export * as reviews from "./reviews";
--- pages/index/sections/Reviews/index.js
+++ pages/index/sections/Reviews/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 
+import { reviews } from "config";
+
 import { Container } from "components/grid/index";
 import Stars from "components/ui/Stars/index";
 
-const ReviewsSection = () => (
-    <section className="section section__reviews bg-white-grey separator-bt">
-        <Container>
-            <div className="block text-center">
-                <Stars amount={5} className="block__elem" />
-                <h3 className="block__title block__elem--lg">
-                    The reviews are in.<br />
-                    <span className="text-normal">People love Memurai</span>
-                </h3>
-            </div>
-        </Container>
-    </section>
-);
+const ReviewsSection = () =>
+    reviews && reviews.length
+        ? (
+            <section className="section section__reviews bg-white-grey separator-bt">
+                <Container>
+                    <div className="block text-center">
+                        <Stars amount={5} className="block__elem" />
+                        <h3 className="block__title block__elem--lg">
+                            The reviews are in.<br />
+                            <span className="text-normal">People love Memurai</span>
+                        </h3>
+                    </div>
+                </Container>
+            </section>
+        )
+        : null;
 
 export default ReviewsSection;
