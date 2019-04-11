c317e4042b079cac0d1163f878de87961dbeca93
Alexis Campailla <alexis@janeasystems.com>
2019-04-11 23:23:49 +0200

    Add social to about us

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,12 @@
 import Layout from "components/Layout"
 
 import PromoSection from "./sections/Promo";
+import SocialSection from "../contact/sections/Social";
 
 const AboutPage = () => (
     <Layout theme="white">
         <PromoSection />
+        <SocialSection />
     </Layout>
 );
 
