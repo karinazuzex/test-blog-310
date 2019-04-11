6adfea996b184049c356be7ae5e82959b2740a65
Alexis Campailla <alexis@janeasystems.com>
2019-04-11 13:25:31 +0200

    Fix CloudFront key

--- server/config/index.js
+++ server/config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     smtp_port: getVar("smtp_port", "465"),
     smtp_username: getVar("smtp_username", "apikey"),
     smtp_password: getVar("smtp_password", "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"),
-    aws_key_pair_id: getVar("aws_key_pair_id", "AKIA3LC2RKLTO3CGYZZW"),
+    aws_key_pair_id: getVar("aws_key_pair_id", "APKAIT4F3FZ745CLKYHQ"),
     aws_private_key_path: getVar("aws_private_key_string", "server/config/aws-pk.pem"),
     aws_file_path: getVar("aws_file_path", "https://dmig02f1dvad2.cloudfront.net/hello-v1.txt"),
     aws_expire_timeout: getVar("aws_expire_timeout", 48 * 60),
