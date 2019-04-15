27c017a82025cd350c36eba03252fd86e6097367
Alexis Campailla <alexis@janeasystems.com>
2019-04-15 11:45:18 +0200

    Make dist file an MSI (still for testing)

--- server/config/index.js
+++ server/config/index.js
@@ -16,7 +16,7 @@ module.exports = {
     smtp_password: getVar("smtp_password", "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"),
     aws_key_pair_id: getVar("aws_key_pair_id", "APKAIT4F3FZ745CLKYHQ"),
     aws_private_key_path: getVar("aws_private_key_string", "server/config/aws-pk.pem"),
-    aws_file_path: getVar("aws_file_path", "https://dmig02f1dvad2.cloudfront.net/hello-v1.txt"),
+    aws_file_path: getVar("aws_file_path", "https://dmig02f1dvad2.cloudfront.net/node-v10.15.3-x86.msi"),
     aws_expire_timeout: getVar("aws_expire_timeout", 48 * 60),
     crypto_key: getVar("crypto_key", "test_crypto_key"),
     memurai_base_url: getVar(
