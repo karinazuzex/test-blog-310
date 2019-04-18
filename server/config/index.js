f42f1d64b8f8f35ac141e1020b082343a3211f57
Alexis Campailla <alexis@janeasystems.com>
2019-04-18 20:24:00 +0200

    Update Cloudfront dev config

--- server/config/index.js
+++ server/config/index.js
@@ -14,9 +14,9 @@ module.exports = {
     smtp_port: getVar("smtp_port", "465"),
     smtp_username: getVar("smtp_username", "apikey"),
     smtp_password: getVar("smtp_password", "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"),
-    aws_key_pair_id: getVar("aws_key_pair_id", "APKAIT4F3FZ745CLKYHQ"),
+    aws_key_pair_id: getVar("aws_key_pair_id", "APKAICHXWYVO32V3JGLA"),
     aws_private_key_path: getVar("aws_private_key_string", "server/config/aws-pk.pem"),
-    aws_file_path: getVar("aws_file_path", "https://dmig02f1dvad2.cloudfront.net/node-v10.15.3-x86.msi"),
+    aws_file_path: getVar("aws_file_path", "https://dmig02f1dvad2.cloudfront.net/test-dummy.msi"),
     aws_expire_timeout: getVar("aws_expire_timeout", 48 * 60),
     crypto_key: getVar("crypto_key", "test_crypto_key"),
     memurai_base_url: getVar(
