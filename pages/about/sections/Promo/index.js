56ec649c21013d28c078a1e8f9fee7c8b5d4ef66
Alexis Campailla <alexis@janeasystems.com>
2019-04-19 12:39:03 +0200

    Update about us

--- pages/about/sections/Promo/index.js
+++ pages/about/sections/Promo/index.js
@@ -10,11 +10,12 @@ const PromoSection = () => (
                     About Us
                 </h3>
                 <p className="block__description block__description--fixed">
-                    The Memurai team is a group of experienced IT professionals with a long track record of delivering
-                    cutting edge solutions to software developers and enterprise customers. Our journey with Memurai for
-                    Windows can be traced back to 2013, when we began leading the development of the port of
-                    Redis to Windows by Microsoft Open Technologies (MS Open Tech). Memurai for Windows is the fruit of the
-                    expertise gained through that project.<br /><br />
+                    The Memurai team is a group of experienced developers and IT professionals with a long track record of delivering
+                    cutting edge solutions to software developers and enterprise customers. We combine our expertise in open source
+                    technologies with deep understanding of the needs and challenges that enterprise customers are facing in the new
+                    fast-moving world of cloud-native app and service development.
+                    Our journey with Memurai for Windows can be traced back to 2013, when we began leading the development the Redis
+                    on Windows project by Microsoft Open Technologies (MSOpenTech).<br /><br />
 
                     Memurai is a division and d/b/a of Janea Systems, Inc.
                 </p>
