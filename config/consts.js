0c2109ce040deafbc436d52d98ffc7a651327482
Alexis Campailla <alexis@janeasystems.com>
2019-05-02 21:40:14 +0200

    Use separate mailchimp lists for local testing

--- config/consts.js
+++ config/consts.js
@@ -1,10 +1,18 @@
 import env from "./env";
 
-export const MAILCHIMP_SUBSCRIBE_URL = "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=c705de38dc";
-export const MAILCHIMP_SUBSCRIBE_NAME = "b_5788bc3268a4b1be403817cce_c705de38dc";
+export const MAILCHIMP_SUBSCRIBE_URL = (env.NODE_ENV === "development")
+    ? "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=d12c100c47"
+    : "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=c705de38dc";
+export const MAILCHIMP_SUBSCRIBE_NAME = (env.NODE_ENV === "development")
+    ? "b_5788bc3268a4b1be403817cce_d12c100c47"
+    : "b_5788bc3268a4b1be403817cce_c705de38dc";
 
-export const MAILCHIMP_DOWNLOAD_URL = "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=3c32c7ca53";
-export const MAILCHIMP_DOWNLOAD_NAME = "b_5788bc3268a4b1be403817cce_3c32c7ca53";
+export const MAILCHIMP_DOWNLOAD_URL = (env.NODE_ENV === "development")
+    ? "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=b1466ddd5d"
+    : "https://janeasystems.us16.list-manage.com/subscribe/post?u=5788bc3268a4b1be403817cce&amp;id=3c32c7ca53";
+export const MAILCHIMP_DOWNLOAD_NAME = (env.NODE_ENV === "development")
+    ? "b_5788bc3268a4b1be403817cce_b1466ddd5d"
+    : "b_5788bc3268a4b1be403817cce_3c32c7ca53";
 
 export const RECAPTCHA_SITE_KEY = env.NODE_ENV === "development"
     ? "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
