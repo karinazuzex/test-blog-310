6cb268e83f885d515d703dffbb2db4b69059760f
Serhii Slobodian <beinc.dn@gmail.com>
2019-05-06 02:05:42 +0300

    download: remove _blank link open to avoid popup blocker

--- pages/download/index.js
+++ pages/download/index.js
@@ -81,7 +81,6 @@ class DownloadPage extends Component {
     download = () => {
         const downloadLink = document.createElement("a");
         downloadLink.href = this.state.link;
-        downloadLink.target = "_blank";
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
