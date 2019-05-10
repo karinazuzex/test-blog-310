465292674abc9d0778f133ac9133ac591c46867e
Serhii Slobodian <beinc.dn@gmail.com>
2019-05-10 19:15:59 +0300

    tooltips: rebuild on component mount #75

--- components/Social/index.js
+++ components/Social/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NextLink from "next/link";
+import ReactTooltip from "react-tooltip";
 
 import { consts, routes, analytics } from "config";
 
@@ -8,83 +9,94 @@ import { Row } from "components/grid";
 import { Link } from "components/ui";
 import { FacebookLogo, LinkedInLogo, NewsletterLogo, TwitterLogo } from "svg";
 
-const Social = ({ theme = "default" }) => {
-    let tooltipTheme;
-    switch (theme) {
-        case "transparent":
-            tooltipTheme = "tooltip-dark";
-            break;
-        default:
-            tooltipTheme = "tooltip-light";
+class Social extends Component {
+    componentDidMount() {
+        ReactTooltip.rebuild();
     }
-    return (
-        <Row className={`justify-center-xs text-center social ${theme ? `social--${theme}` : ""}`}>
-            <Link
-                href={consts.TWITTER_LINK}
-                rel="noreferrer noopener"
-                target="_blank"
-                className="social__item social__item--twitter"
-                onClick={() => {
-                    analytics.event({
-                        category: "External link",
-                        action: "Open",
-                        label: "Twitter"
-                    });
-                }}
-                data-tip="Twitter"
-                data-for={tooltipTheme}
-            >
-                <TwitterLogo />
-            </Link>
-            <Link
-                href={consts.LINKEDIN_LINK}
-                rel="noreferrer noopener"
-                target="_blank"
-                className="social__item social__item--linkedin"
-                onClick={() => {
-                    analytics.event({
-                        category: "External link",
-                        action: "Open",
-                        label: "LinkedIn"
-                    });
-                }}
-                data-tip="LinkedIn"
-                data-for={tooltipTheme}
-            >
-                <LinkedInLogo />
-            </Link>
-            <Link
-                href={consts.FACEBOOK_LINK}
-                rel="noreferrer noopener"
-                target="_blank"
-                className="social__item social__item--facebook"
-                onClick={() => {
-                    analytics.event({
-                        category: "External link",
-                        action: "Open",
-                        label: "Facebook"
-                    });
-                }}
-                data-tip="Facebook"
-                data-for={tooltipTheme}
-            >
-                <FacebookLogo />
-            </Link>
-            <NextLink href={routes.NEWSLETTER_PAGE.path} passHref prefetch>
+
+    render() {
+        const { theme } = this.props;
+        let tooltipTheme;
+        switch (theme) {
+            case "transparent":
+                tooltipTheme = "tooltip-dark";
+                break;
+            default:
+                tooltipTheme = "tooltip-light";
+        }
+        return (
+            <Row className={`justify-center-xs text-center social ${theme ? `social--${theme}` : ""}`}>
                 <Link
-                    className="social__item social__item--newsletter"
-                    data-tip="Subscribe to our newsletter"
+                    href={consts.TWITTER_LINK}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                    className="social__item social__item--twitter"
+                    onClick={() => {
+                        analytics.event({
+                            category: "External link",
+                            action: "Open",
+                            label: "Twitter"
+                        });
+                    }}
+                    data-tip="Twitter"
                     data-for={tooltipTheme}
                 >
-                    <NewsletterLogo />
+                    <TwitterLogo />
                 </Link>
-            </NextLink>
-        </Row>
-    );
-};
+                <Link
+                    href={consts.LINKEDIN_LINK}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                    className="social__item social__item--linkedin"
+                    onClick={() => {
+                        analytics.event({
+                            category: "External link",
+                            action: "Open",
+                            label: "LinkedIn"
+                        });
+                    }}
+                    data-tip="LinkedIn"
+                    data-for={tooltipTheme}
+                >
+                    <LinkedInLogo />
+                </Link>
+                <Link
+                    href={consts.FACEBOOK_LINK}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                    className="social__item social__item--facebook"
+                    onClick={() => {
+                        analytics.event({
+                            category: "External link",
+                            action: "Open",
+                            label: "Facebook"
+                        });
+                    }}
+                    data-tip="Facebook"
+                    data-for={tooltipTheme}
+                >
+                    <FacebookLogo />
+                </Link>
+                <NextLink href={routes.NEWSLETTER_PAGE.path} passHref prefetch>
+                    <Link
+                        className="social__item social__item--newsletter"
+                        data-tip="Subscribe to our newsletter"
+                        data-for={tooltipTheme}
+                    >
+                        <NewsletterLogo />
+                    </Link>
+                </NextLink>
+            </Row>
+        );
+    };
+}
 
 Social.propTypes = {
     theme: PropTypes.oneOf(["transparent", "default"]),
+};
+
+Social.defaultProps = {
+    theme: "default",
 };
 
 export default Social;
