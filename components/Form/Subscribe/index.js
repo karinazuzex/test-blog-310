a5fab1543d84f9c6aef5b2d9f4a9c81fcd7fb868
Serhii Slobodian <beinc.dn@gmail.com>
2019-06-15 02:12:08 +0300

    recaptcha: update bahavior on the rest forms

--- components/Form/Subscribe/index.js
+++ components/Form/Subscribe/index.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import NextLink from "next/link";
 import { info, error, removeAll } from "react-notification-system-redux";
-import ReCaptcha from "react-google-recaptcha";
+import ReCaptcha from "components/ReCaptcha";
 
 import { validators, helpers } from "utils";
-import { consts, exceptions, types, routes } from "config";
+import { consts, exceptions, types, routes, analytics } from "config";
 import { mailerOperations, mailerTypes } from "modules/mailer";
 
 import { Row } from "components/grid";
 import { Button, Checkbox, Link } from "components/ui";
-import {analytics} from "../../../config";
 
 class SubscribeForm extends Component {
     constructor(props) {
@@ -22,19 +20,15 @@ class SubscribeForm extends Component {
             agreementError: null,
             emailError: null,
             processing: false,
+            recaptchaValue: null,
+            recaptchaLoaded: false,
         });
 
         this.state = this.getInitialState();
     }
 
-    componentDidMount() {
-        ReactDOM.render(
-            <ReCaptcha
-                ref={(ref) => { this.recaptcha = ref }}
-                size="invisible"
-                sitekey={consts.RECAPTCHA_SITE_KEY}
-            />,
-            this.recaptchaWrapper);
+    componentWillUnmount() {
+        this.recaptcha.reset();
     }
 
     reset = () => {
@@ -67,6 +61,21 @@ class SubscribeForm extends Component {
         }
     };
 
+    onRecaptchaLoad = () => {
+        this.setState({
+            recaptchaLoaded: true,
+        });
+    };
+
+    onRecaptchaChange = (value) => {
+        this.setState({
+            recaptchaValue: value,
+        });
+        if (this.state.processing) {
+            this.submit(value);
+        }
+    };
+
     handleSubmit = async (e) => {
         e.preventDefault();
         analytics.event({
@@ -74,12 +83,36 @@ class SubscribeForm extends Component {
             action: "Submit",
             label: "Start",
         });
-        this.recaptcha.execute();
         const { dispatch } = this.props;
+        dispatch(removeAll());
         this.setState({
             processing: true,
         });
-        dispatch(removeAll());
+        if (!this.state.recaptchaValue) {
+            this.recaptcha.execute();
+        } else {
+            this.submit(this.state.recaptchaValue);
+        }
+    };
+
+    submit = async (token) => {
+        const { dispatch } = this.props;
+        if (!token) {
+            analytics.event({
+                category: "Subscribe form",
+                action: "Submit",
+                label: exceptions.RECAPTCHA_VALIDATION_FAILED,
+            });
+            dispatch(error({
+                position: "bc",
+                autoDismiss: 0,
+                message: exceptions.RECAPTCHA_VALIDATION_FAILED,
+            }));
+            this.setState({
+                processing: false,
+            });
+            return;
+        }
         const agreement = this.agreement.getValue();
         const email = this.email.value.trim();
         const agreementError = validators.validateAgreement(agreement);
@@ -123,58 +156,63 @@ class SubscribeForm extends Component {
         this.setState({
             processing: false,
         });
-        this.recaptcha.reset();
     };
 
     render() {
-        const disabled = this.state.processing;
+        const disabled = this.state.processing || !this.state.recaptchaLoaded;
         return (
-            <form className="form form--subscribe" onSubmit={this.handleSubmit}>
-                <Row className="form__row align-end-xs">
-                    <div className="form__group">
-                        <label className="form__group-label" htmlFor="email-subscribe">Email address</label>
-                        <input
-                            type="text"
-                            className={`input ${this.state.emailError ? "input--error" : ""}`}
-                            id="email-subscribe"
-                            disabled={disabled}
-                            onChange={this.handleEmailChange}
-                            ref={(ref) => { this.email = ref }}
-                        />
-                    </div>
-                </Row>
-                <Row className="form__row align-center-xs justify-center-xs">
-                    <div className="form__group form__group--checkbox">
-                        <Checkbox
-                            ref={(ref) => {this.agreement = ref}}
-                            onChange={this.handleAgreementChange}
-                        >
-                            I agree to the&nbsp;
-                            <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
-                                <Link theme="red">
-                                    {routes.TERMS_PAGE.nameLong}
-                                </Link>
-                            </NextLink> and&nbsp;
-                            <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
-                                <Link theme="red">
-                                    {routes.PRIVACY_PAGE.nameLong}
-                                </Link>
-                            </NextLink>
-                        </Checkbox>
-                    </div>
-                    <div className="form__group form__group--fixed form__group--lg">
-                        <Button
-                            role="submit"
-                            disabled={disabled}
-                            type="solid"
-                            theme="red-white"
-                        >
-                            Susbcribe
-                        </Button>
-                    </div>
-                </Row>
-                <div ref={(ref) => { this.recaptchaWrapper = ref }} />
-            </form>
+            <Fragment>
+                <form className="form form--subscribe" onSubmit={this.handleSubmit}>
+                    <Row className="form__row align-end-xs">
+                        <div className="form__group">
+                            <label className="form__group-label" htmlFor="email-subscribe">Email address</label>
+                            <input
+                                type="text"
+                                className={`input ${this.state.emailError ? "input--error" : ""}`}
+                                id="email-subscribe"
+                                disabled={disabled}
+                                onChange={this.handleEmailChange}
+                                ref={(ref) => { this.email = ref }}
+                            />
+                        </div>
+                    </Row>
+                    <Row className="form__row align-center-xs justify-center-xs">
+                        <div className="form__group form__group--checkbox">
+                            <Checkbox
+                                ref={(ref) => {this.agreement = ref}}
+                                onChange={this.handleAgreementChange}
+                            >
+                                I agree to the&nbsp;
+                                <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
+                                    <Link theme="red">
+                                        {routes.TERMS_PAGE.nameLong}
+                                    </Link>
+                                </NextLink> and&nbsp;
+                                <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
+                                    <Link theme="red">
+                                        {routes.PRIVACY_PAGE.nameLong}
+                                    </Link>
+                                </NextLink>
+                            </Checkbox>
+                        </div>
+                        <div className="form__group form__group--fixed form__group--lg">
+                            <Button
+                                role="submit"
+                                disabled={disabled}
+                                type="solid"
+                                theme="red-white"
+                            >
+                                Susbcribe
+                            </Button>
+                        </div>
+                    </Row>
+                </form>
+                <ReCaptcha
+                    onLoad={this.onRecaptchaLoad}
+                    onChange={this.onRecaptchaChange}
+                    ref={(ref) => { this.recaptcha = ref }}
+                />
+            </Fragment>
         );
     }
 }
