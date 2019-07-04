81c6b26de039e722b19ef2ac9310b58b388bb547
Alexis Campailla <alexis@janeasystems.com>
2019-07-04 23:52:49 +0200

    Revert "gtm: switch approach"
    This reverts commit d06aaf1ea5af57bf72fd7589e7bba1c0c27a2331.

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Document, { Head, Main, NextScript } from "next/document";
-import Iframe from "react-iframe";
 
 import { GTM_TRACKING_ID } from "config/consts";
 
@@ -10,23 +9,28 @@ export default class extends Document {
             <html>
                 <Head>
                 {GTM_TRACKING_ID ? (
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');
-                            `
-                        }}
-                    />
+                    <Fragment>
+                        <script
+                            async
+                            src={`https://www.googletagmanager.com/gtag/js?id=${GTM_TRACKING_ID}`}
+                        />
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', '${GTM_TRACKING_ID}');
+                                `
+                            }}
+                        />
+                    </Fragment>
                 ) : null}
                 </Head>
                 <body>
                 {GTM_TRACKING_ID ? (
                     <noscript>
-                        <Iframe
+                        <iframe
                             src={`https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}`}
                             height="0"
                             width="0"
