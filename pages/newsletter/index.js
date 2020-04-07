eeb839cdb6ba4039a533f516b3ff422bbf37aa16
Dmitry Luchko <dluchko@zuzex.com>
2020-04-07 16:53:11 +0300

    Include the "Social Follow Memurai"

--- pages/newsletter/index.js
+++ pages/newsletter/index.js
@@ -1,10 +1,11 @@
 import Layout from "components/Layout"
 
 import PromoSection from "./sections/Promo";
-
+import SocialSection from "pages/contact/sections/Social";
 const NewsletterPage = () => (
     <Layout theme="white">
         <PromoSection />
+        <SocialSection />
     </Layout>
 );
 
