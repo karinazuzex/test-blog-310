f0a30f77d9a40cfa912991d903fa44eb2a461f28
dluchko <dluchko@zuzex.com>
2020-04-21 09:54:12 +0300

    fix: BLOG-44 fixed <ol> and <ul> styling inside the blog posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,183 +1,175 @@
-
-
-import Layout from "components/Layout"
+import Layout from "components/Layout";
 import BlogPage from "components/Blog/BlogPage";
-import { withRouter } from 'next/router';
+import { withRouter } from "next/router";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
-import ErrorMessage from 'components/Blog/ErrorMessage';
+import ErrorMessage from "components/Blog/ErrorMessage";
 import SocialSection from "pages/contact/sections/Social";
 
 const GET_POSTS = gql`
-
-    query allArticles($category: String!, $author: String!, $first: IntType, $skip: IntType) {
-
-        categories: allArticles {
-            category
-        }
-
-        allArticles(filter: { category: { matches: { pattern: $category } }, author: {matches: { pattern: $author } } }, first: $first, skip: $skip, orderBy: [publishDateOverride_DESC, _firstPublishedAt_DESC]) {
-            id
-            title
-            description {
-              description
-              title
-            }
-            category
-            body
-            slug
-            headerImage {
-              url
-            }
-            footerImage {
-              url
-            }
-            _firstPublishedAt
-            publishDateOverride
-            author
-        }
-        _allArticlesMeta(filter: { category: { matches: { pattern: $category } }, author: {matches: { pattern: $author } } }) {
-            count
-        }
+  query allArticles(
+    $category: String!
+    $author: String!
+    $first: IntType
+    $skip: IntType
+  ) {
+    categories: allArticles {
+      category
     }
+
+    allArticles(
+      filter: {
+        category: { matches: { pattern: $category } }
+        author: { matches: { pattern: $author } }
+      }
+      first: $first
+      skip: $skip
+      orderBy: [publishDateOverride_DESC, _firstPublishedAt_DESC]
+    ) {
+      id
+      title
+      description {
+        description
+        title
+      }
+      category
+      body
+      slug
+      headerImage {
+        url
+      }
+      footerImage {
+        url
+      }
+      _firstPublishedAt
+      publishDateOverride
+      author
+    }
+    _allArticlesMeta(
+      filter: {
+        category: { matches: { pattern: $category } }
+        author: { matches: { pattern: $author } }
+      }
+    ) {
+      count
+    }
+  }
 `;
 
 const POSTS_PER_PAGE = 5;
 
 const Blog = (props) => {
+  let currentPage = 1;
+  const {
+    router,
+    router: {
+      query: { page, category, author },
+    },
+  } = props;
+  let queryAuthor = "";
+  let queryCategory = "";
 
-    let currentPage = 1;
-
-    const { router, router: { query: { page, category, author} } } = props;
-
-    let queryAuthor = '';
-    let queryCategory = '';
-
-    if (router) {
-        if (author) {
-            queryAuthor = author;
-        }
-        if (category) {
-                queryCategory = category;
-        }
-        if (page) {
-            const queryPage = Number (page);
-            
-            if (typeof queryPage === 'number' && queryPage >= 1) {
-                currentPage = queryPage;
-            }
-        }
+  if (router) {
+    if (author) {
+      queryAuthor = author;
     }
-
-    const needToSkip = (currentPage - 1) * 5;
-
-    // return (
-    //     <Query 
-    //         query={GET_POSTS}
-    //         variables={{
-    //             first: POSTS_PER_PAGE, 
-    //             skip: needToSkip,
-    //             category: queryCategory,
-    //             author: queryAuthor
-    //         }}
-    //         notifyOnNetworkStatusChange
-    //     >
-    //         {({ loading, error, data, fetchMore }) => {
-    //             if (loading) {
-    
-    //                 <Layout theme="white">
-    //                 </Layout>
-    //             }
-            
-    //             if (error) {
-                    
-    //                 return (
-    //                     <ErrorMessage/>
-    //                 )
-    //             }
-            
-    //             if (!data.categories.some(cat => cat.category === category) && category ||
-    //                 !data.allArticles.some(item => item.author === author) && author) {
-        
-    //                 return (
-    //                     <ErrorMessage
-    //                         category={category}
-    //                         author={author}
-    //                     />
-    //                 )
-    //             }
-                
-    //             return (
-    //                 <Layout theme="white">
-    //                     <BlogPage
-    //                         router={props.router}
-    //                         data={data} 
-    //                         currentPage={currentPage}
-    //                     />
-    //                 </Layout>
-    //             )
-                
-    //         }}
-    //     </Query>
-    // )
-
-    const { loading, error, data, fetchMore } = useQuery(GET_POSTS, {
-        variables: { 
-            first: POSTS_PER_PAGE, 
-            skip: needToSkip,
-            category: queryCategory,
-            author: queryAuthor
-        },
-        notifyOnNetworkStatusChange: true
-    });
-
-    if (loading) {
-        
-        <Layout theme="white">
-        </Layout>
+    if (category) {
+      queryCategory = category;
     }
+    if (page) {
+      const queryPage = Number(page);
 
-    if (error) {
-        
-        return (
-            <ErrorMessage/>
-        )
+      if (typeof queryPage === "number" && queryPage >= 1) {
+        currentPage = queryPage;
+      }
     }
+  }
 
-    if (data) {
+  const needToSkip = (currentPage - 1) * 5;
 
-        if (!data.categories.some(cat => cat.category === category) && category ||
-            !data.allArticles.some(item => item.author === author) && author) {
+  // Requesting the posts data.
+  const { loading, error, data, fetchMore } = useQuery(GET_POSTS, {
+    variables: {
+      first: POSTS_PER_PAGE,
+      skip: needToSkip,
+      category: queryCategory,
+      author: queryAuthor,
+    },
+    notifyOnNetworkStatusChange: true,
+  });
 
-            return (
-                <ErrorMessage
-                    category={category}
-                    author={author}
-                />
-            )
-        }
-        
-        return (
-            <Layout theme="white">
-                <BlogPage
-                    router={props.router}
-                    data={data} 
-                    currentPage={currentPage}
-                />
-                <SocialSection />
-            </Layout>
-        )
+  if (loading) {
+    <Layout theme="white"></Layout>;
+  }
+  if (error) {
+    return <ErrorMessage />;
+  }
+  if (data) {
+    if (
+      (!data.categories.some((cat) => cat.category === category) && category) ||
+      (!data.allArticles.some((item) => item.author === author) && author)
+    ) {
+      return <ErrorMessage category={category} author={author} />;
     }
-
-    
-
-
     return (
-        
-        <Layout theme="white">
-        </Layout>
-        
-    )
-}
+      <Layout theme="white">
+        <BlogPage router={props.router} data={data} currentPage={currentPage} />
+        <SocialSection />
+      </Layout>
+    );
+  }
+
+  return <Layout theme="white"></Layout>;
+};
 
 export default withRouter(Blog);
+
+// return (
+//     <Query
+//         query={GET_POSTS}
+//         variables={{
+//             first: POSTS_PER_PAGE,
+//             skip: needToSkip,
+//             category: queryCategory,
+//             author: queryAuthor
+//         }}
+//         notifyOnNetworkStatusChange
+//     >
+//         {({ loading, error, data, fetchMore }) => {
+//             if (loading) {
+
+//                 <Layout theme="white">
+//                 </Layout>
+//             }
+
+//             if (error) {
+
+//                 return (
+//                     <ErrorMessage/>
+//                 )
+//             }
+
+//             if (!data.categories.some(cat => cat.category === category) && category ||
+//                 !data.allArticles.some(item => item.author === author) && author) {
+
+//                 return (
+//                     <ErrorMessage
+//                         category={category}
+//                         author={author}
+//                     />
+//                 )
+//             }
+
+//             return (
+//                 <Layout theme="white">
+//                     <BlogPage
+//                         router={props.router}
+//                         data={data}
+//                         currentPage={currentPage}
+//                     />
+//                 </Layout>
+//             )
+
+//         }}
+//     </Query>
+// )
