8a2b56b895efd461431365197a98b48b235c3628
dluchko <dluchko@zuzex.com>
2020-04-21 18:38:17 +0300

    fix: BLOG-45 NPR! added data parser to BlockItem component (blog cards at /blog page)

--- components/Blog/BlogItems/index.js
+++ components/Blog/BlogItems/index.js
@@ -2,10 +2,13 @@
 import Arrow from 'svg/Arrow';
 import { Link } from "components/ui";
 import NextLink from "next/link";
-        
+import marked from 'marked';
+import ReactHtmlParser from 'react-html-parser';
+import ImageZoom from 'react-medium-image-zoom';
+
 const BlockItem = ({data, router: { pathname, query}}) => data.allArticles.map((item) => {
 
-    const { _firstPublishedAt, publishDateOverride, title, category, author, id, body, slug } = item;    
+    const { _firstPublishedAt, publishDateOverride, title, category, author, id, body, slug } = item;
 
     const optionsCreate = {
         year: 'numeric',
@@ -14,12 +17,34 @@ const BlockItem = ({data, router: { pathname, query}}) => data.allArticles.map((
     }
 
     const text = body.slice(0, 160).split(' ').slice(0,-1).join(' ').trim() + '...';
-
+    const rawMarkup = marked(text);
+    const tranform = (node, index) => {
+        if (node.type === 'tag' && node.name === 'img') {
+            const imgSrc = node.attribs.src || ''
+            return <ImageZoom
+                        key={index}
+                        image={{
+                            src: imgSrc,
+                        }}
+                        zoomImage={{
+                            src: imgSrc,
+                            className: 'blog__image--zoom'
+                        }}
+                        defaultStyles={{
+                            zoomContainer: { background: '#999999'},
+                            overlay: { background: '#4A4A4A'},
+                        }}
+                    />
+        }
+    }
+    const parserOptions = {
+        decodeEntities: true,
+        transform: tranform
+    }
+    let convertedHtml = ReactHtmlParser(rawMarkup, parserOptions);
     const dateCreate = new Date(publishDateOverride || _firstPublishedAt).toLocaleString("en-US", optionsCreate);
 
     const time = Math.ceil(body.split(/\s/).length / 200);
-                
-
     return (
         <div className="block__blog block__elem--40" key={slug}>
             <NextLink href={{ pathname: 'blog/[slug]', query: { item }}} as={`/blog/${slug}`} passHref prefetch>
@@ -40,7 +65,7 @@ const BlockItem = ({data, router: { pathname, query}}) => data.allArticles.map((
             </p>
             <NextLink href={{ pathname: 'blog/[slug]', query: { item }}} as={`/blog/${slug}`} passHref prefetch>
                 <Link>
-                    <p className="blog__text blog__text--bottom blog__text--link">{text}</p>
+                    <p className="blog__text blog__text--bottom blog__text--link">{convertedHtml}</p>
                 </Link>
             </NextLink>
             
