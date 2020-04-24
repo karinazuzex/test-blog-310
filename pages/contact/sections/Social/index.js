b8cdec187e452b5cf53223b0ce4808d43b10ca82
dluchko <dluchko@zuzex.com>
2020-04-24 12:08:49 +0300

    fix: BLOG-47 changed header margin and section separator style at /get-memurai

--- pages/contact/sections/Social/index.js
+++ pages/contact/sections/Social/index.js
@@ -1,22 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container } from "components/grid";
 import Social from "components/Social";
 
-const PromoSection = () => (
-    <section className="section section__social section__social--contact bg-white-grey separator-bt">
-        <Container>
-            <div className="block text-center">
-                <div className="block__pretitle block__elem--xs">
-                    SOCIAL
-                </div>
-                <h3 className="block__title block__elem--lg">
-                    Follow Memurai
-                </h3>
-                <Social theme="transparent" />
-            </div>
-        </Container>
-    </section>
+const PromoSection = ({ className = "" }) => (
+  <section
+    className={`section section__social section__social--contact bg-white-grey separator-bt ${className}`}
+  >
+    <Container>
+      <div className="block text-center">
+        <div className="block__pretitle block__elem--xs">SOCIAL</div>
+        <h3 className="block__title block__elem--lg">Follow Memurai</h3>
+        <Social theme="transparent" />
+      </div>
+    </Container>
+  </section>
 );
+
+PromoSection.propTypes = {
+  className: PropTypes.string,
+};
 
 export default PromoSection;
