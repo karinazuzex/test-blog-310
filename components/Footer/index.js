9a0d468d48ca3b3aecaddfb68caae157cb382d01
dluchko <dluchko@zuzex.com>
2020-04-24 11:11:43 +0300

    fix: BLOG-47 changed footer layout to put subscribe section between links and logo in mobile layout

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -11,78 +11,96 @@ import { LogoFooter } from "svg";
 import Social from "components/Social";
 
 const Footer = () => (
-    <footer role="content-info" className="footer bg-black">
-        <Container>
-            <Row className="footer__row footer__row-main">
-            <Col attributes="xs-12 lg-8" className="footer__col">
-            <Row className="footer__row">
-                <Col attributes="xs-12 lg-3" className="footer__col">
-                    <Column className="align-center-xs align-start-lg">
-                        <div className="footer__header">
-                            Quick links
-                        </div>
-                        <QuickLinks />
-                    </Column>
-                </Col>
-                <Col attributes="xs-12 lg-3" className="footer__col">
-                    <Column className="align-center-xs align-start-lg">
-                        <div className="footer__header">
-                            Learn
-                        </div>
-                        <Learn />
-                    </Column>
-                </Col>
-                <Col attributes="xs-12 lg-4" className="footer__col">
-                    <Column className="align-center-xs align-start-lg">
-                        <div className="footer__header">
-                        Get in touch
-                        </div>
-                        <GetInTouch />
-                    </Column>
-                </Col>
-                <Col attributes="xs-12 lg-12" className="footer__col">
-                    <Column className="align-center-xs align-start-lg footer__contacts-column">
-                        <NextLink href={routes.HOME_PAGE.path} passHref prefetch>
-                            <Link className="header__logo">
-                                <LogoFooter className={`header__logo--img`} />
-                            </Link>
-                        </NextLink>
-                        <div className="footer__contacts">
-                            Memurai 113 Cherry Street #11630 Seattle, WA 98104 <br />
-                            © 2019-2020 All rights reserved by Janea Systems, Inc.<br />
-                            <span className="footer__terms">
-                                <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
-                                    <Link theme="grey-light">
-                                        {routes.TERMS_PAGE.name}
-                                    </Link>
-                                </NextLink>
-                                <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
-                                    <Link theme="grey-light">
-                                        {routes.PRIVACY_PAGE.name}
-                                    </Link>
-                                </NextLink>
-                                <NextLink href={routes.COOKIE_PAGE.path} passHref prefetch>
-                                    <Link theme="grey-light">
-                                        {routes.COOKIE_PAGE.name}
-                                    </Link>
-                                </NextLink>
-                            </span>
-                        </div>
-                    </Column>
-                </Col>
-            </Row>    
+  <footer role="content-info" className="footer bg-black">
+    <Container>
+      <Row className="footer__row footer__row-main">
+        <Col attributes="xs-12 lg-8" className="footer__col">
+          <Row className="footer__row">
+            <Col attributes="xs-12 lg-3" className="footer__col">
+              <Column className="align-center-xs align-start-lg">
+                <div className="footer__header">Quick links</div>
+                <QuickLinks />
+              </Column>
+            </Col>
+            <Col attributes="xs-12 lg-3" className="footer__col">
+              <Column className="align-center-xs align-start-lg">
+                <div className="footer__header">Learn</div>
+                <Learn />
+              </Column>
             </Col>
             <Col attributes="xs-12 lg-4" className="footer__col">
-                <Column className="align-center-xs align-start-lg">
-                    <div className="footer__header">
-                    Subscribe to our newsletter
-                    </div>
-                    <SubscribeForm/>
-                </Column>
+              <Column className="align-center-xs align-start-lg">
+                <div className="footer__header">Get in touch</div>
+                <GetInTouch />
+              </Column>
             </Col>
-            </Row>
-        </Container>
-    </footer>
+            <Col
+              attributes="xs-12 lg-12"
+              className="footer__col footer__col--nomobile "
+            >
+              <Column className="align-center-xs align-start-lg footer__contacts-column">
+                <NextLink href={routes.HOME_PAGE.path} passHref prefetch>
+                  <Link className="header__logo">
+                    <LogoFooter className={`header__logo--img`} />
+                  </Link>
+                </NextLink>
+                <div className="footer__contacts">
+                  Memurai 113 Cherry Street #11630 Seattle, WA 98104 <br />
+                  © 2019-2020 All rights reserved by Janea Systems, Inc.
+                  <br />
+                  <span className="footer__terms">
+                    <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
+                      <Link theme="grey-light">{routes.TERMS_PAGE.name}</Link>
+                    </NextLink>
+                    <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
+                      <Link theme="grey-light">{routes.PRIVACY_PAGE.name}</Link>
+                    </NextLink>
+                    <NextLink href={routes.COOKIE_PAGE.path} passHref prefetch>
+                      <Link theme="grey-light">{routes.COOKIE_PAGE.name}</Link>
+                    </NextLink>
+                  </span>
+                </div>
+              </Column>
+            </Col>
+          </Row>
+        </Col>
+        <Col attributes="xs-12 lg-4" className="footer__col">
+          <Column className="align-center-xs align-start-lg">
+            <div className="footer__header">Subscribe to our newsletter</div>
+            <SubscribeForm />
+          </Column>
+        </Col>
+        <Col
+          attributes="xs-12 lg-12"
+          className="footer__col footer__col--onlymobile "
+        >
+          <Column className="align-center-xs align-start-lg footer__contacts-column">
+            <NextLink href={routes.HOME_PAGE.path} passHref prefetch>
+              <Link className="header__logo">
+                <LogoFooter className={`header__logo--img`} />
+              </Link>
+            </NextLink>
+            <div className="footer__contacts">
+              Memurai 113 Cherry Street #11630 Seattle, WA 98104 <br />
+              © 2019-2020 All rights reserved by Janea Systems, Inc.
+              <br />
+              <span className="footer__terms">
+                <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
+                  <Link theme="grey-light">{routes.TERMS_PAGE.name}</Link>
+                </NextLink>
+                <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
+                  <Link theme="grey-light">{routes.PRIVACY_PAGE.name}</Link>
+                </NextLink>
+                <NextLink href={routes.COOKIE_PAGE.path} passHref prefetch>
+                  <Link theme="grey-light">{routes.COOKIE_PAGE.name}</Link>
+                </NextLink>
+              </span>
+            </div>
+          </Column>
+        </Col>
+      </Row>
+    </Container>
+  </footer>
 );
 
 export default Footer;
