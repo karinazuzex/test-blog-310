80ae013dd9be71b54e02f6fecf8b8552bfe33c10
dluchko <dluchko@zuzex.com>
2020-04-27 20:50:33 +0300

    fix: BLOG-52 Separated Contacts component

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,7 +4,12 @@ import NextLink from "next/link";
 import { routes } from "config";
 
 import { Container, Col, Row, Column } from "components/grid";
-import { QuickLinks, Learn, GetInTouch } from "components/Menu/Footer";
+import {
+  QuickLinks,
+  Learn,
+  GetInTouch,
+  Contacts,
+} from "components/Menu/Footer";
 import SubscribeForm from "components/Form/Subscribe";
 import { Link } from "components/ui";
 import { LogoFooter } from "svg";
@@ -38,29 +43,7 @@ const Footer = () => (
               attributes="xs-12 lg-12"
               className="footer__col footer__col--nomobile "
             >
-              <Column className="align-center-xs align-start-lg footer__contacts-column">
-                <NextLink href={routes.HOME_PAGE.path} passHref prefetch>
-                  <Link className="header__logo">
-                    <LogoFooter className={`header__logo--img`} />
-                  </Link>
-                </NextLink>
-                <div className="footer__contacts">
-                  Memurai 113 Cherry Street #11630 Seattle, WA 98104 <br />
-                  © 2019-2020 All rights reserved by Janea Systems, Inc.
-                  <br />
-                  <span className="footer__terms">
-                    <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
-                      <Link theme="grey-light">{routes.TERMS_PAGE.name}</Link>
-                    </NextLink>
-                    <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
-                      <Link theme="grey-light">{routes.PRIVACY_PAGE.name}</Link>
-                    </NextLink>
-                    <NextLink href={routes.COOKIE_PAGE.path} passHref prefetch>
-                      <Link theme="grey-light">{routes.COOKIE_PAGE.name}</Link>
-                    </NextLink>
-                  </span>
-                </div>
-              </Column>
+              <Contacts />
             </Col>
           </Row>
         </Col>
@@ -74,29 +57,7 @@ const Footer = () => (
           attributes="xs-12 lg-12"
           className="footer__col footer__col--onlymobile "
         >
-          <Column className="align-center-xs align-start-lg footer__contacts-column">
-            <NextLink href={routes.HOME_PAGE.path} passHref prefetch>
-              <Link className="header__logo">
-                <LogoFooter className={`header__logo--img`} />
-              </Link>
-            </NextLink>
-            <div className="footer__contacts">
-              Memurai 113 Cherry Street #11630 Seattle, WA 98104 <br />
-              © 2019-2020 All rights reserved by Janea Systems, Inc.
-              <br />
-              <span className="footer__terms">
-                <NextLink href={routes.TERMS_PAGE.path} passHref prefetch>
-                  <Link theme="grey-light">{routes.TERMS_PAGE.name}</Link>
-                </NextLink>
-                <NextLink href={routes.PRIVACY_PAGE.path} passHref prefetch>
-                  <Link theme="grey-light">{routes.PRIVACY_PAGE.name}</Link>
-                </NextLink>
-                <NextLink href={routes.COOKIE_PAGE.path} passHref prefetch>
-                  <Link theme="grey-light">{routes.COOKIE_PAGE.name}</Link>
-                </NextLink>
-              </span>
-            </div>
-          </Column>
+          <Contacts />
         </Col>
       </Row>
     </Container>
