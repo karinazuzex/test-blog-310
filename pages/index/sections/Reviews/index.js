a4256994c3257795b7941bf028d13d834af4b48b
dluchko <dluchko@zuzex.com>
2020-04-29 14:17:34 +0300

    fix: BLOG-48 added window.event(resize) to initialize Swiper correctly; ReviewsSection changed to a component

--- pages/index/sections/Reviews/index.js
+++ pages/index/sections/Reviews/index.js
@@ -8,56 +8,67 @@ import { Stars } from "components/ui";
 
 import UserPromo from "components/User/Promo";
 
-const ReviewsSection = () => {
-  const swiper = new Swiper(".swiper-container", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  return reviews && reviews.length ? (
-    <section className="section section__reviews bg-white-grey separator-bt">
-      <Container>
-        <div className="block text-center">
-          <Stars amount={5} className="block__elem" />
-          <h3 className="block__title block__elem--lg">
-            The reviews are in.
-            <br />
-            <span className="text-normal">People love Memurai</span>
-          </h3>
-        </div>
-        <div className="swiper-container">
-          <div className="swiper-wrapper">
-            {reviews.map(({ id, text, link, image, logo, name, position }) => (
-              <div className="swiper-slide" style={{ width: "100%" }} key={id}>
-                <Fragment>
-                  <section className="section section__review-short">
-                    <Container>
-                      <div className="block text-center">
-                        <p className="block__elem text-light">
-                          &quot;{text}&quot;
-                        </p>
-                      </div>
-                      <UserPromo
-                        name={name}
-                        position={position}
-                        imagePath={image}
-                        logoPath={logo}
-                        link={link}
-                      />
-                    </Container>
-                  </section>
-                </Fragment>
-              </div>
-            ))}
-          </div>
+class ReviewsSection extends React.Component {
+  state = {};
 
-          <div className="swiper-button-next"></div>
-          <div className="swiper-button-prev"></div>
-        </div>
-      </Container>
-    </section>
-  ) : null;
-};
+  componentDidMount() {
+    const swiper = new Swiper(".swiper-container", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    this.setState({ swiper });
+    window.dispatchEvent(new Event("resize"));
+  }
+
+  render() {
+    return reviews && reviews.length ? (
+      <section className="section section__reviews bg-white-grey separator-bt">
+        <Container>
+          <div className="block text-center">
+            <Stars amount={5} className="block__elem" />
+            <h3 className="block__title block__elem--lg">
+              The reviews are in.
+              <br />
+              <span className="text-normal">People love Memurai</span>
+            </h3>
+          </div>
+          <div className="swiper-container">
+            <div className="swiper-wrapper">
+              {reviews.map(
+                ({ id, text, link, image, logo, name, position }) => (
+                  <div className="swiper-slide" key={id}>
+                    <Fragment>
+                      <section className="section section__review-short">
+                        <Container>
+                          <div className="block text-center">
+                            <p className="block__elem text-light">
+                              &quot;{text}&quot;
+                            </p>
+                          </div>
+                          <UserPromo
+                            name={name}
+                            position={position}
+                            imagePath={image}
+                            logoPath={logo}
+                            link={link}
+                          />
+                        </Container>
+                      </section>
+                    </Fragment>
+                  </div>
+                )
+              )}
+            </div>
+
+            <div className="swiper-button-next"></div>
+            <div className="swiper-button-prev"></div>
+          </div>
+        </Container>
+      </section>
+    ) : null;
+  }
+}
 
 export default ReviewsSection;
