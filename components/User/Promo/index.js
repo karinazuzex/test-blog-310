a062e12e9774f979ad95eebd4ef820dfd41ca3cc
dluchko <dluchko@zuzex.com>
2020-05-01 16:14:19 +0300

    fix: BLOG-56 slider footer adaptive styles fix

--- components/User/Promo/index.js
+++ components/User/Promo/index.js
@@ -12,39 +12,44 @@ const UserPromo = ({
   logoPath,
   link,
 }) => (
-  <Row theme="no-col" className="user-promo justify-center-xs">
-    {imagePath ? (
-      <Fragment>
-        <img
-          src={imagePath}
-          className="user-promo__icon"
-          alt="Customer photo"
-        />
-      </Fragment>
-    ) : (
-      <Fragment>
-        <div className="user-promo__icon"></div>
-      </Fragment>
-    )}
-    <Column className="justify-center-xs user-promo__info">
-      <div className="user-promo__info-name">{name}</div>
-      <div className="user-promo__info-position">{position}</div>
-    </Column>
-    {logoPath ? (
-      <Fragment>
-        <NextLink href={link} passHref prefetch>
-          <Link theme="grey-light" className="f-menu__link">
-            <img
-              src={logoPath}
-              alt="Company logo"
-              className="user-promo__company"
-            />
-          </Link>
-        </NextLink>
-      </Fragment>
-    ) : (
-      <div className="user-promo__company">StepUp</div>
-    )}
+  <Row theme="no-col" className="user-promo justify-center-xs align-center-xs">
+    <Row className="col-xs-12 col-sm-6 col-lg-4 justify-center-xs">
+      {imagePath ? (
+        <Fragment>
+          <img
+            src={imagePath}
+            className="user-promo__icon"
+            alt="Customer photo"
+          />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <div className="user-promo__icon"></div>
+        </Fragment>
+      )}
+      <Column className="justify-center-xs user-promo__info">
+        <div className="user-promo__info-name">{name}</div>
+        <div className="user-promo__info-position">{position}</div>
+      </Column>
+    </Row>
+
+    <Row className="col-xs-12 col-sm-6 col-lg-4 justify-center-xs">
+      {logoPath ? (
+        <Fragment>
+          <NextLink href={link} passHref prefetch>
+            <Link theme="grey-light" className="f-menu__link">
+              <img
+                src={logoPath}
+                alt="Company logo"
+                className="user-promo__company"
+              />
+            </Link>
+          </NextLink>
+        </Fragment>
+      ) : (
+        <div className="user-promo__company">StepUp</div>
+      )}
+    </Row>
   </Row>
 );
 
