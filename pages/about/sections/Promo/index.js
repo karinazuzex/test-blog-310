5c823abf8042e29ecc1a69b8a44aa6f0ae54e918
dluchko <dluchko@zuzex.com>
2020-05-06 11:20:19 +0300

    fix: BLOG-59 about us word wrapping fixed

--- pages/about/sections/Promo/index.js
+++ pages/about/sections/Promo/index.js
@@ -10,15 +10,15 @@ const PromoSection = () => (
                     About Us
                 </h3>
                 <p className="block__description block__description--fixed">
-                    The Memurai team is a group of experienced developers and IT<br/> 
-                    professionals with a long track record of delivering cutting edge solutions to<br/>
-                    software developers and enterprise customers. We combine our expertise in<br/>
-                    open source technologies with deep understanding of the needs and<br/>
-                    challenges that enterprise customers are facing in the new fast-moving<br/>
-                    world of cloud-native app and service development.<br/><br/>
-                    Our journey with Memurai for Windows can be traced back to 2013, when we<br/>
-                    began leading the development of the Redis on Windows project by<br/>
-                    Microsoft Open Technologies (MSOpenTech).<br /><br />
+                    The Memurai team is a group of experienced developers and IT <br/>
+                    professionals with a long track record of delivering cutting edge solutions to <br/>
+                    software developers and enterprise customers. We combine our expertise in <br/>
+                    open source technologies with deep understanding of the needs and <br/>
+                    challenges that enterprise customers are facing in the new fast-moving <br/>
+                    world of cloud-native app and service development. <br className="br-mobile" /><br className="br-mobile" />
+                    Our journey with Memurai for Windows can be traced back to 2013, when we <br/>
+                    began leading the development of the Redis on Windows project by <br/>
+                    Microsoft Open Technologies (MSOpenTech).<br className="br-mobile" /><br  className="br-mobile"  />
 
                     <small>Memurai is a division and d/b/a of Janea Systems, Inc.</small>
                 </p>
