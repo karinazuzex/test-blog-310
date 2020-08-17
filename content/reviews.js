be41926388a26c337acbf88d90223a1a63cb42bf
vavdeev <vavdeev@zuzex.com>
2020-08-17 17:59:54 +0300

    BLOG-68: optimized images for main page

--- content/reviews.js
+++ content/reviews.js
@@ -3,7 +3,8 @@ export default [
     id: 0,
     text:
       "The solution we built has a SaaS and an on-prem component. After struggling with Redis on Windows version 3.x, which was out of life support for a while we switched to Memurai which is compatible with Redis 5. We got a very good surprise and the issues we struggled with dried up overnight. I am happy to recommend Memurai. Try it out yourself and see if this works for you.",
-    image: "static/images/testimonials/Julian-Wilde.jpeg",
+    image: "static/images/testimonials/Julian-Wilde_origin.jpeg",
+    image_90x90: "static/images/testimonials/Julian-Wilde_90x90.png",
     logo: "static/images/testimonials/wormly.png",
     link: "//wormly.com",
     name: "Julian Wilde",
@@ -13,7 +14,8 @@ export default [
     id: 1,
     text:
       "Our company generates large amounts of proprietary data especially images which are served to our customers. We explored many alternatives and found out that Memurai was the optimal solution for our needs. Thank you Memurai team for helping us!",
-    image: "static/images/testimonials/Max-Ciccotosto.jpg",
+    image: "static/images/testimonials/Max-Ciccotosto_origin.jpg",
+    image_90x90: "static/images/testimonials/Max-Ciccotosto_90x90.png",
     logo: "static/images/testimonials/mediarails.png",
     link: "//mediarails.com",
     name: "Max Ciccotosto",
@@ -23,7 +25,8 @@ export default [
     id: 2,
     text:
       "Migrating from an old legacy hardware and software system to the Microsoft stack was an interesting journey. Our in-house developers are well versed and trained on Windows, .net, Azure and all the other tools available from the Microsoft ecosystem. We explored several alternatives and come to the conclusion that Memurai is the most effective way to solve our engineering goals. The Memurai developers showed that they know what they are doing. I am looking forward to working with the Memurai team for years to come.",
-    image: "static/images/testimonials/Steve-McLeod.jpg",
+    image: "static/images/testimonials/Steve-McLeod_origin.jpg",
+    image_90x90: "static/images/testimonials/Steve-McLeod_90x90.jpg",
     logo: "static/images/testimonials/feature-upwote.png",
     link: "//featureupvote.com",
     name: "Steve McLeod",
--- next.config.js
+++ next.config.js
@@ -3,54 +3,56 @@ const withImages = require('next-images');
 const { getPosts } = require("./get-posts");
 
 module.exports = withImages({
-    webpack(config, { defaultLoaders }) {
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
+  inlineImageLimit: 5000,
 
-        if (entries["main.js"]) {
-          entries["main.js"].unshift("./polyfills.js");
-        }
-        return entries;
+  webpack(config, { defaultLoaders }) {
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (entries["main.js"]) {
+        entries["main.js"].unshift("./polyfills.js");
+      }
+      return entries;
+    };
+
+    return config;
+  },
+
+  publicRuntimeConfig: {
+    NODE_ENV: process.env.NODE_ENV,
+    GTM_TRACKING_ID: process.env.GTM_TRACKING_ID,
+    WEBSITE_DOMAIN: process.env.WEBSITE_DOMAIN,
+    MEMURAI_VERSION: process.env.MEMURAI_VERSION,
+    MEMURAI_VERSION_SHORT: process.env.MEMURAI_VERSION_SHORT,
+    DOWNLOAD_BUTTON_TEXT: process.env.DOWNLOAD_BUTTON_TEXT,
+    REDIS_COMPAT_FULLVER: process.env.REDIS_COMPAT_FULLVER,
+    SHOW_DRAFTS : process.env.SHOW_DRAFTS,
+    DISABLE_COVID19 : process.env.DISABLE_COVID19
+  },
+
+  async exportPathMap() {
+    const pathMap = {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/faq": { page: "/faq" },
+      "/get-memurai": { page: "/get-memurai" },
+      "/download": { page: "/download" },
+      "/contact": { page: "/contact" },
+      "/newsletter": { page: "/newsletter" },
+      "/terms": { page: "/terms" },
+      "/privacy": { page: "/privacy" },
+      "/cookie": { page: "/cookie" },
+      "/blog": { page: "/blog" },
+    };
+
+    (await getPosts()).map((post) => {
+      pathMap[`/blog/${post.link}`] = {
+        page: `/blog/[slug]`,
+        query: { slug: post.link },
       };
+    });
 
-      return config;
-    },
-
-    publicRuntimeConfig: {
-      NODE_ENV: process.env.NODE_ENV,
-      GTM_TRACKING_ID: process.env.GTM_TRACKING_ID,
-      WEBSITE_DOMAIN: process.env.WEBSITE_DOMAIN,
-      MEMURAI_VERSION: process.env.MEMURAI_VERSION,
-      MEMURAI_VERSION_SHORT: process.env.MEMURAI_VERSION_SHORT,
-      DOWNLOAD_BUTTON_TEXT: process.env.DOWNLOAD_BUTTON_TEXT,
-      REDIS_COMPAT_FULLVER: process.env.REDIS_COMPAT_FULLVER,
-      SHOW_DRAFTS : process.env.SHOW_DRAFTS,
-      DISABLE_COVID19 : process.env.DISABLE_COVID19
-    },
-
-    async exportPathMap() {
-      const pathMap = {
-        "/": { page: "/" },
-        "/about": { page: "/about" },
-        "/faq": { page: "/faq" },
-        "/get-memurai": { page: "/get-memurai" },
-        "/download": { page: "/download" },
-        "/contact": { page: "/contact" },
-        "/newsletter": { page: "/newsletter" },
-        "/terms": { page: "/terms" },
-        "/privacy": { page: "/privacy" },
-        "/cookie": { page: "/cookie" },
-        "/blog": { page: "/blog" },
-      };
-
-      (await getPosts()).map((post) => {
-        pathMap[`/blog/${post.link}`] = {
-          page: `/blog/[slug]`,
-          query: { slug: post.link },
-        };
-      });
-
-      return pathMap;
-    },
-  });
+    return pathMap;
+  },
+});
