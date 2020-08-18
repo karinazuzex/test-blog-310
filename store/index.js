18cf1b5ea769a0b7689557c301368440f4a0af38
vavdeev <vavdeev@zuzex.com>
2020-08-18 17:08:50 +0300

    BLOG-68: reworked redux connect. Added static render pages

--- store/index.js
+++ store/index.js
@@ -1,10 +1,61 @@
+import { useMemo } from 'react'
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 
-import rootReducer from "reducers";
+import rootReducer from "../reducers";
 
-const persistentState = {};
+let store;
+
+const logger = createLogger({
+  collapsed: true,
+  level: "info",
+});
+
+const enhancer = process.env.NODE_ENV === "development"
+  ? applyMiddleware(thunk, logger)
+  : applyMiddleware(thunk);
+
+function initStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    enhancer
+  );
+}
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState);
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') {
+    return _store;
+  }
+  // Create the store once in the client
+  if (!store) {
+    store = _store;
+  }
+
+  return _store;
+};
+
+export function useStore(initialState) {
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
+}
+
+/* const persistentState = {};
 
 const logger = createLogger({
     collapsed: true,
@@ -15,8 +66,8 @@ let enhancer = process.env.NODE_ENV === "development"
     ? applyMiddleware(thunk, logger)
     : applyMiddleware(thunk);
 
-export const initializeStore = (initialState = JSON.parse(JSON.stringify(persistentState))) => createStore(
+export const initializeStore = (initialState = {}) => createStore(
     rootReducer,
     initialState,
     enhancer,
-);
+); */
