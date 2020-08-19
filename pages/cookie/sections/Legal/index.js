0427f012a114b375b3fbb754c0b1c8e194f1c49e
vavdeev <vavdeev@zuzex.com>
2020-08-19 09:46:29 +0300

    BLOG-68: fixed cookie policy page. P.S. on localhost will show error message, on main domain should work correct

--- pages/cookie/sections/Legal/index.js
+++ pages/cookie/sections/Legal/index.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import { useRef, useEffect } from "react";
 
 import { Container } from "components/grid";
 
-class LegalSection extends Component {
-  render() {
-    return (
-      <section className="section section__legal section__legal--cookie">
-        <Container>
-          <div className="legal">
-            <div className="legal-block">
-              <h3 className="legal-header">Cookie policy</h3>
-            </div>
-            <div className="legal-block">
-              <script
-                id="CookieDeclaration"
-                src="https://consent.cookiebot.com/ad4b92a8-bdac-4756-852d-30e83d1feba3/cd.js"
-                type="text/javascript"
-                async
-              ></script>
-            </div>
+function LegalSection() {
+  const content = useRef(null);
+
+  useEffect(() => {
+    const cookieScript = document.createElement("script");
+    cookieScript.type = "text/javascript";
+    cookieScript.async = true;
+    cookieScript.src = "https://consent.cookiebot.com/ad4b92a8-bdac-4756-852d-30e83d1feba3/cd.js";
+    content.current.appendChild(cookieScript);
+  }, []);
+
+  return (
+    <section className="section section__legal section__legal--cookie">
+      <Container>
+        <div className="legal">
+          <div className="legal-block">
+            <h3 className="legal-header">Cookie policy</h3>
           </div>
-        </Container>
-      </section>
-    );
-  }
+          <div className="legal-block" ref={content} />
+        </div>
+      </Container>
+    </section>
+  );
 }
 
 export default LegalSection;
