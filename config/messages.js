0514eb88f1615b418a7665893ba4b47723c4422b
vavdeev <vavdeev@zuzex.com>
2020-10-23 17:34:54 +0300

    BLOG: 81. issue 234: Added name and email on form notification after success form send

--- config/messages.js
+++ config/messages.js
@@ -3,3 +3,11 @@ export const REQUEST_SUCCESSFULLY_SENT = "Your request has been sent successfull
 
 // Successfull subscription
 export const SUBSCRIPTION_SUCCESS = "You have been successfully subscribed.";
+
+/**
+ * @param {string} name user name
+ * @param {string} email user email
+ */
+export function requestSuccessfullySent(name, email) {
+  return `Thanks ${name} for your interest in Memurai. We will contact you soon at ${email}`;
+}
